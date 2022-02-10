28ad6edabca7af8192553b65290f16ef4bbc2349
alex junior <alexjuniorvalera1020@gmail.com>
2022-02-09 22:45:06 -0400

    Add project files.

---
+++ FinanzasPersonales(G)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinanzasPersonales_G_.MvcApplication" Language="C#" %>
---
+++ FinanzasPersonales(G)/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FinanzasPersonales_G_.Startup))]
+namespace FinanzasPersonales_G_
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
